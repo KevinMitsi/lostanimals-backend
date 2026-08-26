cbd4e03f30080466bc2dd8e6a5667f0d91033892
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2026-08-26 09:59:31 -0500

    Changes flyway

--- src/main/java/io/github/KevinMitsi/animalesperdidos/infrastructure/config/PersonalDataProtectionConfiguration.java
+++ src/main/java/io/github/KevinMitsi/animalesperdidos/infrastructure/config/PersonalDataProtectionConfiguration.java
@@ -1,8 +1,6 @@
 package io.github.KevinMitsi.animalesperdidos.infrastructure.config;
 
-import io.github.KevinMitsi.animalesperdidos.infrastructure.adapter.persistence.migration.EncryptExistingPersonalDataMigration;
 import io.github.KevinMitsi.animalesperdidos.infrastructure.adapter.security.PersonalDataCipher;
-import org.springframework.boot.flyway.autoconfigure.FlywayConfigurationCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +11,5 @@ public class PersonalDataProtectionConfiguration {
     @Bean
     PersonalDataCipher personalDataCipher(PersonalDataProtectionProperties properties) {
         return new PersonalDataCipher(properties.getEncryptionKey(), properties.getLookupKey());
-    }
-
-    @Bean
-    FlywayConfigurationCustomizer personalDataMigration(PersonalDataCipher cipher) {
-        return configuration -> configuration.javaMigrations(new EncryptExistingPersonalDataMigration(cipher));
     }
 }
